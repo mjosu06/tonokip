ae2af4f7917ee3774d692e6aa45fafd56d08d32e
mjosu06 <00001111189328sp@al.educacao.sp.gov.br>
2024-10-08 10:30:13 -0300

    consertando

--- Tonokip_Firmware/pins.h
+++ Tonokip_Firmware/pins.h
@@ -241,21 +241,21 @@
 #error Oops!  Make sure you have 'Arduino Duemilanove w/ ATMega328' selected from the 'Tools -> Boards' menu.
 #endif
 
-#define X_STEP_PIN         19
-#define X_DIR_PIN          18
-#define X_ENABLE_PIN       -1
-#define X_MIN_PIN          17
+#define X_STEP_PIN         2
+#define X_DIR_PIN.         5
+#define X_ENABLE_PIN       8
+#define X_MIN_PIN          9
 #define X_MAX_PIN          -1
 
-#define Y_STEP_PIN         10
-#define Y_DIR_PIN           7
-#define Y_ENABLE_PIN       -1
-#define Y_MIN_PIN           8
+#define Y_STEP_PIN          3
+#define Y_DIR_PIN           6
+#define Y_ENABLE_PIN        X_ENABLE_PIN       
+#define Y_MIN_PIN.          10
 #define Y_MAX_PIN          -1
 
 #define Z_STEP_PIN          4
 #define Z_DIR_PIN           7
-#define Z_ENABLE_PIN        2
+#define Z_ENABLE_PIN        
 #define Z_MIN_PIN           4
 #define Z_MAX_PIN          -1
 
